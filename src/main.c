ba8a9ab89bfb29e74a96fa107d1fcf7bafd8120d
pancake <pancake@nopcode.org>
2026-01-08 18:56:35 +0100

    No unistd on windows

--- src/main.c
+++ src/main.c
@@ -41,10 +41,11 @@ extern int deflateEnd(z_stream *strm);
 #include <sys/types.h>
 #include <errno.h>
 #include <fcntl.h>
-#include <unistd.h>
 #ifdef _WIN32
 #include <direct.h>
 #include <io.h>
+#else
+#include <unistd.h>
 #endif
 
 #ifndef PATH_MAX
