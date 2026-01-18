27cce22692eed49c0364349c4d9fdaf74fc9e1f1
pancake <pancake@nopcode.org>
2026-01-18 16:03:04 +0100

    Release 0.4.4

--- src/lib/crc32.inc.c
+++ src/lib/crc32.inc.c
@@ -1,4 +1,5 @@
 /* CRC-32 polynomial table (reversed) */
+// clang-format off
 static const uint32_t crc32_table[256] = {
 	0x00000000, 0x77073096, 0xee0e612c, 0x990951ba, 0x076dc419, 0x706af48f, 0xe963a535, 0x9e6495a3,
 	0x0edb8832, 0x79dcb8a4, 0xe0d5e91e, 0x97d2d988, 0x09b64c2b, 0x7eb17cbd, 0xe7b82d07, 0x90bf1d91,
@@ -33,6 +34,7 @@ static const uint32_t crc32_table[256] = {
 	0xbdbdf21c, 0xcabac28a, 0x53b39330, 0x24b4a3a6, 0xbad03605, 0xcdd70693, 0x54de5729, 0x23d967bf,
 	0xb3667a2e, 0xc4614ab8, 0x5d681b02, 0x2a6f2b94, 0xb40bbe37, 0xc30c8ea1, 0x5a05df1b, 0x2d02ef8d
 };
+// clang-format on
 
 /* Calculate CRC-32 checksum (standard reversed polynomial) */
 static uint32_t otezip_crc32(uint32_t crc, const void *buf, size_t len) {
